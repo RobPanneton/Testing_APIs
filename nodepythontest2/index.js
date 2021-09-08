262dd9a28c3c8f3c1692fc1ee84f32ca40d2c1e6
RobPanneton <robert.panneton@gmail.com>
2021-09-08 13:00:17 -0400

    exercise 3, data sent to browser from python!

--- nodepythontest2/index.js
+++ nodepythontest2/index.js
@@ -2,24 +2,22 @@ const express = require("express");
 const { spawn } = require("child_process");
 const app = express();
 const port = 3000;
+
 app.get("/", (req, res) => {
-  var dataToSend;
-  // spawn new child process to call the python script
-  // exercise 2 : add params to spawn method
-  const python = spawn("python", ["script1.py", "node.js", "python"]);
-  // collect data from script
+  var largeDataSet = [];
+  // spawn new child process to call python script
+  const python = spawn("python", ["script3.py"]);
+  //collect data from script
   python.stdout.on("data", function (data) {
     console.log("Pipe data from python script ...");
-    dataToSend = data.toString();
+    largeDataSet.push(data);
   });
-  // in close event we are sure that stream from child process is closed
+  // in close event we are sure that the stream is from child process is closed
   python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
     // send data to browser
-    res.send(dataToSend);
+    res.send(largeDataSet.join(""));
   });
 });
-app.listen(port, () =>
-  console.log(`Example app listening on port 
-${port}!`)
-);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
