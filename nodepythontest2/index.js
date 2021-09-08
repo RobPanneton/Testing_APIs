b295b6d1c2eeba055a6f7bee870d6a3a98e1229e
RobPanneton <robert.panneton@gmail.com>
2021-09-08 11:43:56 -0400

    successfully render first python script through node!

--- nodepythontest2/index.js
+++ nodepythontest2/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { spawn } = require("child_process");
 const app = express();
-const port = 4000;
+const port = 3000;
 app.get("/", (req, res) => {
   var dataToSend;
   // spawn new child process to call the python script
