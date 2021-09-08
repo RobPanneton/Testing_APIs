07a3ad37577ccbcf158fddef3c2c726542b248d6
RobPanneton <robert.panneton@gmail.com>
2021-09-08 16:33:24 -0400

    smoll clean

--- server/yfinance/handlers.js
+++ server/yfinance/handlers.js
@@ -2,19 +2,18 @@ const request = require("request-promise");
 const { spawn } = require("child_process");
 
 const getStockInfo = async (req, res) => {
-  var dataSet = [];
+  var dataSet;
 
   const python = spawn("python", ["./yfinance/getData.py"]);
 
   python.stdout.on("data", (data) => {
     console.log("Pipe data from python script ...");
-    // dataSet.push(data);
     dataSet = data;
   });
 
-  // python.stderr.on("data", (data) => {
-  //   console.error(`stderr: ${data}`);
-  // });
+  python.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
 
   python.on("close", async (code) => {
     console.log(`child process close all stdio with code ${code}`);
