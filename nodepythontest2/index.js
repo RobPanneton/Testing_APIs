684ef337fcfd73a30d6412fbaa1963fd70d6279b
RobPanneton <robert.panneton@gmail.com>
2021-09-08 13:09:01 -0400

    refactor response to return required array to server

--- nodepythontest2/index.js
+++ nodepythontest2/index.js
@@ -12,11 +12,19 @@ app.get("/", (req, res) => {
     console.log("Pipe data from python script ...");
     largeDataSet.push(data);
   });
+
   // in close event we are sure that the stream is from child process is closed
   python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
     // send data to browser
-    res.send(largeDataSet.join(""));
+    // res.send(largeDataSet.join(""));
+    console.log(JSON.parse(largeDataSet));
+    const data = JSON.parse(largeDataSet);
+    res.status(200).json({
+      status: 200,
+      message: "Success!",
+      data: data,
+    });
   });
 });
 
